38d8dd1235bf756b75b6140feafae028a3af80d3
rob duits <rob.duits@incentro.com>
2017-07-28 07:57:52 +0200

    SVG symbols update
    Add SVG symbols task to Gulp + watch task
    Remove gulpfile.js.rob
    Add AMD wrapper in js examples
    Update flexbox scss addon
    Add oneliner scss mixin

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,20 +7,26 @@
 
 // Development folders
 var developmentFolder = 'src/',
-  developmentJsFolder = developmentFolder + 'js/',
-  developmentStylesFolder = developmentFolder + 'styles/',
-  developmentSassFolder = developmentStylesFolder + 'sass/';
+    developmentJsFolder = developmentFolder + 'js/',
+    developmentStylesFolder = developmentFolder + 'styles/',
+    developmentSassFolder = developmentStylesFolder + 'sass/';
 
 // Theme folders
 var themeFolder = 'theme/',
-  themeJsFolder = themeFolder + 'js/',
-  themeStylesFolder = themeFolder + 'styles/',
-  themeCssFolder = themeStylesFolder + 'css/';
+    themeJsFolder = themeFolder + 'js/',
+    themeStylesFolder = themeFolder + 'styles/',
+    themeCssFolder = themeStylesFolder + 'css/';
+
+// SVG folders
+var svgAssets = 'src/svg/*.svg',
+    svgCssFile = 'src/styles/sass/custom/svg-symbols.scss',
+    svgSvgPath = 'theme/images/svg/',
+    svgDest = 'theme/images/';
 
 // Deployment folders
 var deploymentFolder = 'deployment/web/',
-  deploymentJsFolder = deploymentFolder + 'js/',
-  deploymentStylesFolder = deploymentFolder + 'styles/css/';
+    deploymentJsFolder = deploymentFolder + 'js/',
+    deploymentStylesFolder = deploymentFolder + 'styles/css/';
 
 // Browsersync feature, please specify the host & port of the running project (without http://)
 var proxyAddress = 'localhost:8080';
@@ -30,15 +36,16 @@ var proxyAddress = 'localhost:8080';
  * Don't try to edit below this line, unless you know what you are doing *
  *************************************************************************/
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  concat = require('gulp-concat'),
-  rename = require('gulp-rename'),
-  uglify = require('gulp-uglify'),
-  debug = require('gulp-debug'),
-  browserSync = require('browser-sync').create(),
-  path = require('path'),
-  sourcemaps = require('gulp-sourcemaps'),
-  jshint = require('gulp-jshint');
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    debug = require('gulp-debug'),
+    browserSync = require('browser-sync').create(),
+    svgSymbols = require('gulp-svg-symbols'),
+    path = require('path'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer  = require('gulp-autoprefixer');
 
 gulp.task('build-sass', function () {
   return gulp.src(developmentSassFolder + '**/*.scss')
@@ -71,13 +78,6 @@ gulp.task('watch:development-js', function () {
   gulp.watch(developmentJsFolder + '**/*.js', ['build-js']);
 });
 
-gulp.task('jshint', function() {
-  return gulp.src(developmentJsFolder + '**/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
-    // .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
-});
-
 // Gulp tasks
 gulp.task('default', ['watch:sass']);
 gulp.task('build', ['build-sass']);
@@ -89,19 +89,42 @@ gulp.task('browsersync-sass', function () {
     .pipe(sass({
       outputStyle: 'expanded'
     }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(themeCssFolder))
     .pipe(gulp.dest(deploymentStylesFolder))
     .pipe(browserSync.stream());
 });
 
+gulp.task('SVGsymbols', function () {
+  return gulp.src(svgAssets)
+    .pipe(svgSymbols({
+      title: '%f icon',
+      svgClassname: 'generali-icons',
+      templates: [
+        'default-svg',
+        'default-css',
+        'default-demo'
+      ]
+    }))
+    .pipe(gulp.dest(svgDest));
+});
+
 gulp.task('watch:browsersync-sass', function () {
   gulp.watch(developmentSassFolder + '**/*.scss', ['browsersync-sass']);
 });
 
-gulp.task('dev', ['build-js', 'browsersync-sass', 'watch:browsersync-sass', 'watch:development-js'], function () {
+gulp.task('watch:SVGactivity', function () {
+  gulp.watch(svgAssets, ['SVGsymbols']);
+});
+
+gulp.task('dev', ['build-js', 'browsersync-sass', 'SVGsymbols', 'watch:browsersync-sass', 'watch:development-js'], function () {
   browserSync.init({
     proxy: proxyAddress,
+    online: true,
     ws: true
   });
 });
